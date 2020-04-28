af9845daa72f866658238bbb50a86b436897f948
Jae-Son Song <jaesonsong@gmail.com>
2020-04-28 15:11:18 -0400

    Write and test new band form

--- MusicApp/app/controllers/bands_controller.rb
+++ MusicApp/app/controllers/bands_controller.rb
@@ -3,5 +3,34 @@ class BandsController < ApplicationController
     @bands = Band.all
     render :index
   end
-  
+
+  def show
+    @band = Band.find_by(id: params[:id])
+
+    if @band
+      render :show
+    else
+      redirect_to bands_url
+    end
+  end
+
+  def new
+    @band = Band.new
+    render :new
+  end
+
+  def create
+    @band = Band.new(band_params)
+
+    if @band.save
+      redirect_to band_url(@band)
+    else
+      redirect_to new_band_url
+    end    
+  end
+
+  private
+  def band_params
+    params.require(:band).permit(:name)
+  end
 end
