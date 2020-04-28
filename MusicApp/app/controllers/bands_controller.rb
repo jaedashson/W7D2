0c9e7ba499d74e6f1fedf08303094ccd1fc92527
Jae-Son Song <jaesonsong@gmail.com>
2020-04-28 16:03:18 -0400

    Implement bands, Edit band link not working

--- MusicApp/app/controllers/bands_controller.rb
+++ MusicApp/app/controllers/bands_controller.rb
@@ -29,6 +29,33 @@ class BandsController < ApplicationController
     end    
   end
 
+  def edit
+    @band = Band.find_by(id: params[:id])
+
+    if @band
+      render :edit
+    else
+      redirect_to bands_url
+    end
+  end
+
+  def update
+    @band = Band.find_by(id: params[:id])
+
+    if @band.update_attributes(band_params)
+      redirect_to band_url(@band)
+    else
+      redirect_to edit_band_url(@band)
+    end
+  end
+
+  def destroy
+    @band = Band.find_by(id: params[:id])
+    @band.destroy
+
+    redirect_to bands_url
+  end
+
   private
   def band_params
     params.require(:band).permit(:name)
